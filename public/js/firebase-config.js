ccda64291e2f9ae74c75368a0af55725794b3ee9
opsspinbot-sketch <ops.spinbot@gmail.com>
2026-08-08 14:03:16 +0530

    fix: make firebaseConfig support dynamic overrides

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -9,12 +9,12 @@ try {
   const { getAuth } = await import("https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js");
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBL8RJ_pabY71EtNq0HjeNWLtUNE6XoPFQ",
-    authDomain: "invoice-af966.firebaseapp.com",
-    projectId: "invoice-af966",
-    storageBucket: "invoice-af966.firebasestorage.app",
-    messagingSenderId: "583941270564",
-    appId: "1:583941270564:web:ed3d4564c4f444e5e4fcb1"
+    apiKey: window.__FIREBASE_API_KEY__ || "AIzaSyBL8RJ_pabY71EtNq0HjeNWLtUNE6XoPFQ",
+    authDomain: window.__FIREBASE_AUTH_DOMAIN__ || "invoice-af966.firebaseapp.com",
+    projectId: window.__FIREBASE_PROJECT_ID__ || "invoice-af966",
+    storageBucket: window.__FIREBASE_STORAGE_BUCKET__ || "invoice-af966.firebasestorage.app",
+    messagingSenderId: window.__FIREBASE_MESSAGING_SENDER_ID__ || "583941270564",
+    appId: window.__FIREBASE_APP_ID__ || "1:583941270564:web:ed3d4564c4f444e5e4fcb1"
   };
 
   app = initializeApp(firebaseConfig);
